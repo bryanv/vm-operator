7c4b223dac3aadcbf64ee2f3c21d632be2f2ffa2
bryanv <bryanventeicher@gmail.com>
2023-04-03 12:52:49 -0500

    Default CERT_MANAGER_URL for now

--- hack/deploy-local-certmanager.sh
+++ hack/deploy-local-certmanager.sh
@@ -6,6 +6,8 @@ set -o pipefail
 set -o nounset
 set -x
 
+CERT_MANAGER_URL="git::https://gitlab.eng.vmware.com/core-build/cayman_photon/support/install/objects/cert-manager/local?submodules=false&ref=0255b635116f67feb7bc61151a2b7e36f5c77055"
+
 # Exit with a non-zero exit code and an error message if
 # the CERT_MANAGER_URL is not set.
 CERT_MANAGER_URL="${CERT_MANAGER_URL:?}"

3d2e0d2a305a321b3016d1b9519a1313f54f539c
Bryan Venteicher <bryanventeicher@gmail.com>
2023-04-19 14:11:31 -0500

    Add VirtualMachineProviderInterfaceA2 interface
    Hook up fake to the unit test controller context for use with
    start testing the controllers.

--- pkg/vmprovider/interface.go
+++ pkg/vmprovider/interface.go
@@ -12,7 +12,7 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/client"
 
 	vmopv1 "github.com/vmware-tanzu/vm-operator/api/v1alpha1"
-
+	"github.com/vmware-tanzu/vm-operator/api/v1alpha2"
 	imgregv1a1 "github.com/vmware-tanzu/vm-operator/external/image-registry/api/v1alpha1"
 )
 
@@ -38,6 +38,31 @@ type VirtualMachineProviderInterface interface {
 	ListItemsFromContentLibrary(ctx context.Context, contentLibrary *vmopv1.ContentLibraryProvider) ([]string, error)
 	GetVirtualMachineImageFromContentLibrary(ctx context.Context, contentLibrary *vmopv1.ContentLibraryProvider, itemID string,
 		currentCLImages map[string]vmopv1.VirtualMachineImage) (*vmopv1.VirtualMachineImage, error)
+	GetItemFromLibraryByName(ctx context.Context, contentLibrary, itemName string) (*library.Item, error)
+	UpdateContentLibraryItem(ctx context.Context, itemID, newName string, newDescription *string) error
+	SyncVirtualMachineImage(ctx context.Context, cli, vmi client.Object) error
+
+	GetTasksByActID(ctx context.Context, actID string) (tasksInfo []vimTypes.TaskInfo, retErr error)
+}
+
+type VirtualMachineProviderInterfaceA2 interface {
+	CreateOrUpdateVirtualMachine(ctx context.Context, vm *v1alpha2.VirtualMachine) error
+	DeleteVirtualMachine(ctx context.Context, vm *v1alpha2.VirtualMachine) error
+	PublishVirtualMachine(ctx context.Context, vm *v1alpha2.VirtualMachine,
+		vmPub *v1alpha2.VirtualMachinePublishRequest, cl *imgregv1a1.ContentLibrary, actID string) (string, error)
+	GetVirtualMachineGuestHeartbeat(ctx context.Context, vm *v1alpha2.VirtualMachine) (v1alpha2.GuestHeartbeatStatus, error)
+	GetVirtualMachineWebMKSTicket(ctx context.Context, vm *v1alpha2.VirtualMachine, pubKey string) (string, error)
+	GetVirtualMachineHardwareVersion(ctx context.Context, vm *v1alpha2.VirtualMachine) (int32, error)
+
+	CreateOrUpdateVirtualMachineSetResourcePolicy(ctx context.Context, resourcePolicy *v1alpha2.VirtualMachineSetResourcePolicy) error
+	IsVirtualMachineSetResourcePolicyReady(ctx context.Context, availabilityZoneName string, resourcePolicy *v1alpha2.VirtualMachineSetResourcePolicy) (bool, error)
+	DeleteVirtualMachineSetResourcePolicy(ctx context.Context, resourcePolicy *v1alpha2.VirtualMachineSetResourcePolicy) error
+
+	// "Infra" related
+	UpdateVcPNID(ctx context.Context, vcPNID, vcPort string) error
+	ResetVcClient(ctx context.Context)
+	ComputeCPUMinFrequency(ctx context.Context) error
+
 	GetItemFromLibraryByName(ctx context.Context, contentLibrary, itemName string) (*library.Item, error)
 	UpdateContentLibraryItem(ctx context.Context, itemID, newName string, newDescription *string) error
 	SyncVirtualMachineImage(ctx context.Context, cli, vmi client.Object) error

fa18a5c4109ba0b81973a342426a8324d520bcc5
Bryan Venteicher <bryanventeicher@gmail.com>
2023-09-01 11:09:01 -0500

    WIP session rebase remove old GetMergedvAppConfigSpec

--- pkg/vmprovider/providers/vsphere2/session/session_util.go
+++ pkg/vmprovider/providers/vsphere2/session/session_util.go
@@ -7,12 +7,6 @@ import (
 	vimTypes "github.com/vmware/govmomi/vim25/types"
 )
 
-const (
-	// OvfEnvironmentTransportGuestInfo is the OVF transport type that uses
-	// GuestInfo. The other valid type is "iso".
-	OvfEnvironmentTransportGuestInfo = "com.vmware.guestInfo"
-)
-
 func ExtraConfigToMap(input []vimTypes.BaseOptionValue) (output map[string]string) {
 	output = make(map[string]string)
 	for _, opt := range input {
@@ -37,43 +31,4 @@ func MergeExtraConfig(extraConfig []vimTypes.BaseOptionValue, newMap map[string]
 		}
 	}
 	return merged
-}
-
-// GetMergedvAppConfigSpec prepares a vApp VmConfigSpec which will set the vmMetadata supplied key/value fields. Only
-// fields marked userConfigurable and pre-existing on the VM (ie. originated from the OVF Image)
-// will be set, and all others will be ignored.
-func GetMergedvAppConfigSpec(inProps map[string]string, vmProps []vimTypes.VAppPropertyInfo) *vimTypes.VmConfigSpec {
-	outProps := make([]vimTypes.VAppPropertySpec, 0)
-
-	for _, vmProp := range vmProps {
-		if vmProp.UserConfigurable == nil || !*vmProp.UserConfigurable {
-			continue
-		}
-
-		inPropValue, found := inProps[vmProp.Id]
-		if !found || vmProp.Value == inPropValue {
-			continue
-		}
-
-		vmPropCopy := vmProp
-		vmPropCopy.Value = inPropValue
-		outProp := vimTypes.VAppPropertySpec{
-			ArrayUpdateSpec: vimTypes.ArrayUpdateSpec{
-				Operation: vimTypes.ArrayUpdateOperationEdit,
-			},
-			Info: &vmPropCopy,
-		}
-		outProps = append(outProps, outProp)
-	}
-
-	if len(outProps) == 0 {
-		return nil
-	}
-
-	return &vimTypes.VmConfigSpec{
-		Property: outProps,
-		// Ensure the transport is guestInfo in case the VM does not have
-		// a CD-ROM device required to use the ISO transport.
-		OvfEnvironmentTransport: []string{OvfEnvironmentTransportGuestInfo},
-	}
 }
--- pkg/vmprovider/providers/vsphere2/vmlifecycle/bootstrap_vappconfig_test.go
+++ pkg/vmprovider/providers/vsphere2/vmlifecycle/bootstrap_vappconfig_test.go
@@ -7,12 +7,12 @@ import (
 	"strings"
 
 	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/ginkgo/extensions/table"
 	. "github.com/onsi/gomega"
 
+	"github.com/vmware/govmomi/vim25/types"
 	corev1 "k8s.io/api/core/v1"
 	"k8s.io/utils/pointer"
-
-	"github.com/vmware/govmomi/vim25/types"
 
 	vmopv1 "github.com/vmware-tanzu/vm-operator/api/v1alpha2"
 	"github.com/vmware-tanzu/vm-operator/api/v1alpha2/common"
@@ -201,4 +201,67 @@ var _ = Describe("VAppConfig Bootstrap", func() {
 			})
 		})
 	})
+})
+
+var _ = Describe("GetMergedvAppConfigSpec", func() {
+
+	DescribeTable("returns expected props",
+		func(inProps map[string]string, vmProps []types.VAppPropertyInfo, expected *types.VmConfigSpec) {
+			vAppConfigSpec := vmlifecycle.GetMergedvAppConfigSpec(inProps, vmProps)
+			if expected == nil {
+				Expect(vAppConfigSpec).To(BeNil())
+			} else {
+				Expect(vAppConfigSpec.Property).To(HaveLen(len(expected.Property)))
+				for i := range vAppConfigSpec.Property {
+					Expect(vAppConfigSpec.Property[i].Info.Key).To(Equal(expected.Property[i].Info.Key))
+					Expect(vAppConfigSpec.Property[i].Info.Id).To(Equal(expected.Property[i].Info.Id))
+					Expect(vAppConfigSpec.Property[i].Info.Value).To(Equal(expected.Property[i].Info.Value))
+					Expect(vAppConfigSpec.Property[i].ArrayUpdateSpec.Operation).To(Equal(types.ArrayUpdateOperationEdit))
+				}
+				Expect(vAppConfigSpec.OvfEnvironmentTransport).To(HaveLen(1))
+				Expect(vAppConfigSpec.OvfEnvironmentTransport[0]).To(Equal(vmlifecycle.OvfEnvironmentTransportGuestInfo))
+			}
+		},
+		Entry("return nil for absent vm and input props",
+			map[string]string{},
+			[]types.VAppPropertyInfo{},
+			nil,
+		),
+		Entry("return nil for non UserConfigurable vm props",
+			map[string]string{
+				"one-id": "one-override-value",
+				"two-id": "two-override-value",
+			},
+			[]types.VAppPropertyInfo{
+				{Key: 1, Id: "one-id", Value: "one-value"},
+				{Key: 2, Id: "two-id", Value: "two-value", UserConfigurable: pointer.Bool(false)},
+			},
+			nil,
+		),
+		Entry("return nil for UserConfigurable vm props but no input props",
+			map[string]string{},
+			[]types.VAppPropertyInfo{
+				{Key: 1, Id: "one-id", Value: "one-value"},
+				{Key: 2, Id: "two-id", Value: "two-value", UserConfigurable: pointer.Bool(true)},
+			},
+			nil,
+		),
+		Entry("return valid vAppConfigSpec for setting mixed UserConfigurable props",
+			map[string]string{
+				"one-id":   "one-override-value",
+				"two-id":   "two-override-value",
+				"three-id": "three-override-value",
+			},
+			[]types.VAppPropertyInfo{
+				{Key: 1, Id: "one-id", Value: "one-value", UserConfigurable: nil},
+				{Key: 2, Id: "two-id", Value: "two-value", UserConfigurable: pointer.Bool(true)},
+				{Key: 3, Id: "three-id", Value: "three-value", UserConfigurable: pointer.Bool(false)},
+			},
+			&types.VmConfigSpec{
+				Property: []types.VAppPropertySpec{
+					{Info: &types.VAppPropertyInfo{Key: 2, Id: "two-id", Value: "two-override-value"}},
+				},
+			},
+		),
+	)
 })

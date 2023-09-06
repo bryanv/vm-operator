a819af650293854228785d89ba116b97a7b95968
Bryan Venteicher <bryanventeicher@gmail.com>
2023-09-06 16:28:07 -0500

    Update v1a1 webhook names

--- webhooks/virtualmachine/v1alpha1/validation/virtualmachine_validator.go
+++ webhooks/virtualmachine/v1alpha1/validation/virtualmachine_validator.go
@@ -66,7 +66,7 @@ const (
 	invalidNextRestartTimeOnUpdateNow         = "mutation webhooks are required to restart VM"
 )
 
-// +kubebuilder:webhook:verbs=create;update,path=/default-validate-vmoperator-vmware-com-v1alpha1-virtualmachine,mutating=false,failurePolicy=fail,groups=vmoperator.vmware.com,resources=virtualmachines,versions=v1alpha1,name=default.validating.virtualmachine.vmoperator.vmware.com,sideEffects=None,admissionReviewVersions=v1;v1beta1
+// +kubebuilder:webhook:verbs=create;update,path=/default-validate-vmoperator-vmware-com-v1alpha1-virtualmachine,mutating=false,failurePolicy=fail,groups=vmoperator.vmware.com,resources=virtualmachines,versions=v1alpha1,name=default.validating.virtualmachine.v1alpha1.vmoperator.vmware.com,sideEffects=None,admissionReviewVersions=v1;v1beta1
 // +kubebuilder:rbac:groups=vmoperator.vmware.com,resources=virtualmachines,verbs=get;list
 // +kubebuilder:rbac:groups=vmoperator.vmware.com,resources=virtualmachines/status,verbs=get
 
